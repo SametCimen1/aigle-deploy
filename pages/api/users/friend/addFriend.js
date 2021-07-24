7051e921b70a9b37514ad5ab2b14e243996e481a
SametCimen1 <cimensamet338@gmail.com>
2021-07-24 11:58:52 -0400

    fixed module not found util mongodb

--- pages/api/users/friend/addFriend.js
+++ pages/api/users/friend/addFriend.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../../util/mongodb";
+import { connectToDatabase } from "../../../../util/mongodb";
 
 export default async function handler(req,res){
     const {db} = await connectToDatabase();
